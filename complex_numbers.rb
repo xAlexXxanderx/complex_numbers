ade82a8ee971d60f97f189557cb721ae6920e362
Alexander Grechko <alexxxander1111@gmail.com>
2012-11-18 15:33:08 +0200

    Now, the program calculates the squares and cubes of complex numbers also improved design. - and / calculated - the first to the second, added - the second to the first.

--- complex_numbers.rb
+++ complex_numbers.rb
@@ -6,16 +6,28 @@ else
   c2 = Complex(rand(10), rand(10))
 end
 
-puts c1
-puts c2
-puts ""
 s1 = c1.to_s
 s2 = c2.to_s
+puts "z1 = " + s1
+puts "z2 = " + s2
+puts ""
 addition = c1+c2
-puts s1 + " + " + s2 + " = " + addition.to_s
-subtraction = c1-c2
-puts s1 + " - " + s2 + " = " + subtraction.to_s
+puts "z1 + z2 = (" + s1 + ") + (" + s2 + ") = " + addition.to_s
+subtraction1 = c1-c2
+puts "z1 - z2 = (" + s1 + ") - (" + s2 + ") = " + subtraction1.to_s
+subtraction2 = c2-c1
+puts "z2 - z1 = (" + s2 + ") - (" + s1 + ") = " + subtraction2.to_s
 multiplication = c1*c2
-puts s1 + " * " + s2 + " = " + multiplication.to_s
-division = c1/c2
-puts s1 + " / " + s2 + " = " + division.to_s
+puts "z1 * z2 = (" + s1 + ") * (" + s2 + ") = " + multiplication.to_s
+division1 = c1/c2
+puts "z1 / z2 = (" + s1 + ") / (" + s2 + ") = " + division1.to_s
+division2 = c2/c1
+puts "z2 / z1 = (" + s2 + ") / (" + s1 + ") = " + division2.to_s
+square1 = c1**2
+puts "z1^2 = (" + s1 + ")^2 = " + square1.to_s
+square2 = c1**2
+puts "z2^2 = (" + s2 + ")^2 = " + square2.to_s
+cube1 = c2**3
+puts "z1^3 = (" + s1 + ")^3 = " + cube1.to_s
+cube2 = c2**3
+puts "z2^3 = (" + s2 + ")^3 = " + cube2.to_s
